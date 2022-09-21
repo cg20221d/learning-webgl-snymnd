6d3ddbc9a78bb9500a4b9991eb49c75aed2e9c1b
Muhammad Yunus <Yunusenvire@gmail.com>
2022-09-21 15:04:17 +0700

    feat: add movement event

--- main.js
+++ main.js
@@ -3,11 +3,15 @@ const main = () =>{
     const canvas = document.querySelector('#kanvas');
     const gl = canvas.getContext('webgl');
 
+    var x = 0.0;
+    var y = 0.0;
+
+
     const vertices = [
-        0.5, 0.5, 0.0, 1.0, 1.0,
-        0.0, 0.0, 1.0, 0.0, 1.0,
-        -0.5, 0.5, 1.0, 1.0, 0.0,
-        0.0, 1.0, 1.0, 1.0, 1.0
+        0.5 + x, 0.0 + y, 0.0, 1.0, 1.0,
+        0.0, -0.5, 1.0, 0.0, 1.0,
+        -0.5, 0.0, 1.0, 1.0, 0.0,
+        0.0, 0.5, 1.0, 1.0, 1.0
     ];
     
     const buffer = gl.createBuffer();
@@ -22,9 +26,10 @@ const main = () =>{
     attribute vec3 aColor;
     uniform float uTheta;
     varying vec3 vColor;
+    uniform vec2 uTranslate;
     void main() {
-        float x = -sin(uTheta) * aPosition.x + cos(uTheta) * aPosition.y;
-        float y = cos(uTheta) * aPosition.x + sin(uTheta) * aPosition.y;
+        float x = -sin(uTheta) *(aPosition.x)+ cos(uTheta) * (aPosition.y)  + uTranslate.x;
+        float y = cos(uTheta) * (aPosition.x) + sin(uTheta) * (aPosition.y) + uTranslate.y;
         
         gl_Position = vec4(x, y, 0.0, 1.0);
         vColor = aColor;
@@ -55,9 +60,11 @@ const main = () =>{
     // varoaible lokal
     var theta = 0.0;
     var freeze = false;
+    var translate = [0.0, 0.0];
 
     // variable pointer ke GLSL
     var uTheta = gl.getUniformLocation(shaderProgram, 'uTheta');
+    var uTranslate = gl.getUniformLocation(shaderProgram, 'uTranslate');
 
     // bind attribute : told gpu how to collect position value from buffer to  every vertex that processing 
     const aPosition = gl.getAttribLocation(shaderProgram, 'aPosition');
@@ -80,15 +87,53 @@ const main = () =>{
     }
     document.addEventListener('click', onMouseClick);
 
+
+    const onKeyUp = (event) => {
+        if(event.code === 'Space'){
+            // freeze = !freeze;
+        } 
+    }
+    const onKeyDown = (event) => {
+        if(event.code === 'Space'){
+            // freeze = !freeze;
+        }
+        else if (event.code === 'KeyA'){
+            // move left the vertex
+            translate[0] -= 0.1;
+        }
+        else if (event.code === 'KeyD'){
+            // move left the vertex
+            translate[0] += 0.1;
+        }
+        else if (event.code === 'KeyW'){
+            // move left the vertex
+            translate[1] += 0.1;
+        }
+        else if (event.code === 'KeyW'){
+            // move left the vertex
+            translate[1] += 0.1;
+        }
+        else if (event.code === 'KeyS'){
+            // move left the vertex
+            translate[1] -= 0.1;
+        }
+    }
+
+
+    document.addEventListener('keydown', onKeyDown);
+    document.addEventListener('keyup', onKeyUp);
+
+
     // main loop
     const render = () => {
         gl.clearColor(0.0, 0.0, 0.0, 1.0);
         gl.clear(gl.COLOR_BUFFER_BIT);
 
-        if(!freeze){
+        // if(!freeze){
             theta += -0.01;
             gl.uniform1f(uTheta, theta);
-        }
+            gl.uniform2fv(uTranslate, translate);
+        // }
 
         gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
 
